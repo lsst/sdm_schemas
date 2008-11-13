ca3b02e0e73d5c3829bbb0cf5d15e3dd969957f6
smm <smm@git.lsstcorp.org>
2008-11-13 02:10:13 +0000

    Added INSERT statements for small fixed-content lookup tables (prv_Filter, mops_SSMDesc) to pipeline initialization script

--- sql/lsstPipelineSetup4mysql.sql
+++ sql/lsstPipelineSetup4mysql.sql
@@ -7,6 +7,36 @@
 -- See <http://lsstdev.ncsa.uiuc.edu:8100/trac/wiki/Copyrights>
 -- for copyright information.
 
+-- ================================================= --
+-- Populate lookup/mapping tables with fixed content --
+-- ================================================= --
+
+-- LSST filters
+INSERT INTO prv_Filter VALUES (0,  0, 'u', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (1,  0, 'g', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (2,  0, 'r', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (3,  0, 'i', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (4,  0, 'z', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (5,  0, 'y', NULL, 0.0, 0.0);
+-- Additional filters used by MOPS
+INSERT INTO prv_Filter VALUES (6,  0, 'w', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (7,  0, 'V', NULL, 0.0, 0.0);
+INSERT INTO prv_Filter VALUES (-99, 0, 'DD', NULL, 0.0, 0.0); -- dummy filter
+
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'S0', 'MOPS synthetic NEO');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'S1', 'MOPS synthetic main-belt object');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'St', 'MOPS synthetic Trojan');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'SC', 'MOPS synthetic Centaur');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'ST', 'MOPS synthetic trans-Neptunian object');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'SS', 'MOPS synthetic scattered disk object');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'Sc', 'MOPS synthetic comet');
+INSERT INTO mops_SSMDesc (prefix, description) VALUES ( 'SM', 'MOPS synthetic control object');
+
+
+-- ================================================================ --
+-- Create tables in the form (ENGINE/indexes) expected by pipelines --
+-- ================================================================ --
+
 CREATE TABLE DIASourceTemplate LIKE DIASource;
 
 ALTER TABLE DIASourceTemplate
