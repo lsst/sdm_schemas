66a320eba500ab8e522f273333a2175fd29fc4f7
Jacek Becla <becla@slac.stanford.edu>
2012-01-30 12:00:15 -0800

    redid file headers

--- sql/lsstSchema4mysqlDC3a.sql
+++ sql/lsstSchema4mysqlDC3a.sql
@@ -1,11 +1,25 @@
-
--- LSST Database Schema
--- $Author$
--- $Revision$
--- $Date$
+-- LSST Data Management System
+-- Copyright 2008, 2009, 2010 LSST Corporation.
+-- 
+-- This product includes software developed by the
+-- LSST Project (http://www.lsst.org/).
 --
--- See <http://lsstdev.ncsa.uiuc.edu:8100/trac/wiki/Copyrights>
--- for copyright information.
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the LSST License Statement and 
+-- the GNU General Public License along with this program.  If not, 
+-- see <http://www.lsstcorp.org/LegalNotices/>.
+
+
+-- LSST Database Schema, DC3a series
 
 
 CREATE TABLE AAA_Version_3_0_24 (version CHAR);
--- sql/lsstSchema4mysqlPT1_2.sql
+++ sql/lsstSchema4mysqlPT1_2.sql
@@ -1,12 +1,28 @@
-
--- LSST Database Schema
--- $Author$
--- $Revision: 18841 $
--- $Date$
+-- LSST Data Management System
+-- Copyright 2008, 2009, 2010 LSST Corporation.
+-- 
+-- This product includes software developed by the
+-- LSST Project (http://www.lsst.org/).
 --
--- See <http://lsstdev.ncsa.uiuc.edu/trac/wiki/Copyrights>
--- for copyright information.
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the LSST License Statement and 
+-- the GNU General Public License along with this program.  If not, 
+-- see <http://www.lsstcorp.org/LegalNotices/>.
 
+
+-- LSST Database Schema, PT1_2 series
+--
+-- UCD definitions based on:
+-- http://www.ivoa.net/Documents/cover/UCDlist-20070402.html
 
 
 SET FOREIGN_KEY_CHECKS=0;
