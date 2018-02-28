4435e7520151b44bfd75afbe9f82ae74611d82c4
Brian Van Klaveren <bvan@slac.stanford.edu>
2018-02-28 15:06:22 -0800

    Wrap long line

--- sql/lsstSchema4mysql_sdss_pdac.sql
+++ sql/lsstSchema4mysql_sdss_pdac.sql
@@ -162,7 +162,8 @@ CREATE TABLE DeepCoadd_To_Htm10 (
         -- <descr>Pointer to DeepCoadd.</descr>
     htmId10 int(11) NOT NULL,
         -- <ucd>pos.HTM</ucd>
-        -- <descr>ID for Level 10 HTM triangle overlapping exposure. For each exposure in DeepCoadd, there will be one row for every overlapping triangle.</descr>
+        -- <descr>ID for Level 10 HTM triangle overlapping exposure. For each exposure in DeepCoadd, 
+        -- there will be one row for every overlapping triangle.</descr>
     KEY IDX_htmId10 (htmId10),
     KEY IDX_deepCoaddId (deepCoaddId)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
