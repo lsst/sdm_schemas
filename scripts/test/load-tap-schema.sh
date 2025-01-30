d271cf14bbcecc77c14deec35bc8291cd551a839
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-01-30 15:45:18 -0600

    Drop the database or schema between tests to avoid conflicts
    There can be naming conflicts between different schemas when loading
    into TAP_SCHEMA, so completely avoid this by dropping and recreating
    the database or schema after every test.

--- scripts/test/load-tap-schema.sh
+++ scripts/test/load-tap-schema.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+# set -x
+
 # Check if FELIS_ENGINE_URL is set
 if [ -z "$FELIS_ENGINE_URL" ]; then
     echo "Error: FELIS_ENGINE_URL is not set. Please set the environment variable and try again."
@@ -11,48 +14,36 @@ protocol=$(echo "$FELIS_ENGINE_URL" | awk -F '://' '{print $1}')
 
 if [[ "$protocol" == *"mysql"* ]]; then
     database="mysql"
-    FELIS_ENGINE_URL="${FELIS_ENGINE_URL}/TAP_SCHEMA"
 elif [[ "$protocol" == *"postgresql"* ]]; then
     database="postgresql"
 elif [[ "$protocol" == *"sqlite"* ]]; then
     database="sqlite"
     FELIS_ENGINE_URL="sqlite:///TAP_SCHEMA.db"
 else
-    echo "Error: FELIS_ENGINE_URL protocol must contain one of 'mysql', 'postgresql' or 'sqlite'."
+    echo "Error: FELIS_ENGINE_URL protocol must be one of 'mysql', 'postgresql' or 'sqlite'"
     exit 1
 fi
+
+echo "Detected database type: $database"
 
 # Initialize a variable to track if any command fails
 error_occurred=0
 
 for yaml_file in yml/*.yaml; do
     filename=$(basename "$yaml_file")
-    echo "Uploading to TAP_SCHEMA from $yaml_file..."
-    felis --log-level ERROR load-tap --engine-url ${FELIS_ENGINE_URL} "$yaml_file"
-    # Check if the felis command was successful
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to create SQL from $yaml_file"
-      error_occurred=1
-    else
-      echo "Done uploading to TAP_SCHEMA from $yaml_file"
-    fi
+    echo "Loading TAP_SCHEMA from $yaml_file..."
+    felis --log-level ERROR init-tap-schema --engine-url ${FELIS_ENGINE_URL}
+    felis --log-level ERROR load-tap-schema --engine-url ${FELIS_ENGINE_URL} "$yaml_file"
+    # Drop the TAP_SCHEMA database or schema
     if [[ "$database" == "mysql" ]]; then
-        mysql -DTAP_SCHEMA -e "
-            DELETE FROM \`columns\`;
-            DELETE FROM \`key_columns\`;
-            DELETE FROM \`keys\`;
-            DELETE FROM \`schemas\`;
-            DELETE FROM \`tables\`;
-        " &> /dev/null
+        echo "Dropping TAP_SCHEMA mysql database..."
+        mysql -e "DROP DATABASE TAP_SCHEMA;"
     elif [[ "$database" == "postgresql" ]]; then
-        psql -q -c "TRUNCATE TABLE columns, key_columns, keys, schemas, tables;" &> /dev/null
+        echo "Dropping TAP_SCHEMA postgresql schema..."
+        psql -c "DROP SCHEMA \"TAP_SCHEMA\" CASCADE;"
     elif [[ "$database" == "sqlite" ]]; then
-        sqlite3 TAP_SCHEMA.db "DELETE FROM columns;
-            DELETE FROM key_columns;
-            DELETE FROM keys;
-            DELETE FROM schemas;
-            DELETE FROM tables;
-        " &> /dev/null
+        echo "Dropping TAP_SCHEMA sqlite database..."
+        rm TAP_SCHEMA.db
     fi
 done
 
