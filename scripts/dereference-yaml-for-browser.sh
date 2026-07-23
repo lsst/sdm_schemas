513aec707b3ada70c155ca256d7bccf2b02e0f7f
Jeremy McCormick <jeremym@slac.stanford.edu>
2026-07-23 15:38:12 -0500

    Set column ref index increment when generating YAML for the browser

--- scripts/dereference-yaml-for-browser.sh
+++ scripts/dereference-yaml-for-browser.sh
@@ -5,5 +5,7 @@ set -euo pipefail
 mkdir -p browser/yml
 for schema in yml/*.yaml; do
     base=$(basename "$schema")
-    felis --log-level ERROR dump "$schema" "browser/yml/$base" --dereference-resources --strip-ids
+    felis --log-level ERROR --column-ref-index-increment 10 dump \
+        "$schema" "browser/yml/$base" \
+        --dereference-resources --strip-ids
 done
